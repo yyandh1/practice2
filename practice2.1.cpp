c04fe6aa10de38dd16529a1b354935190bbd4fbc
yyandh1 <billchifr1@gmail.com>
2024-04-20 21:30:01 +0300

    учуся

---
+++ practice2.1.cpp
@@ -0,0 +1,120 @@
+﻿#include <iostream>
+#include <cmath>
+#include <vector>
+
+using namespace std;
+
+int NOD(int firstNum, int secondNum) { // определяем наибольший общий делитель
+    if (firstNum % secondNum == 0) {
+        return secondNum;
+    }
+    if (secondNum % firstNum == 0) {
+        return firstNum;
+    }
+    if (firstNum > secondNum) {
+        return NOD(firstNum % secondNum, secondNum);
+    }
+    return NOD(firstNum, secondNum % firstNum);
+}
+
+bool Prime(int a) {
+    if (a == 1) { //единица - не простое число
+        return false;
+    }
+    for (int i = 2; i < sqrt(a) + 1; i++) {
+        if (a % i == 0) {
+            return false;
+        }
+    }
+    return true;
+}
+
+int EilerFunc(int p) { // определяем количество взаимно простых чисел для p 
+    int result = p;
+    for (int i = 2; i * i <= p; i++) { //проходимся до корня из числа
+        if (p % i == 0) { //если число разделилось на i, то мы уменьшаем р на i так как все числа,
+            while (p % i == 0) { // которые делятся на i не являются взаимно простыми с p
+                p /= i;
+            }
+            result -= result / i; // из результата вычитаем количество чисел result/i
+        }
+    }
+    if (p > 1) { // если п - простое число, которое не разделилось, то оно простое и результатом
+        result -= result / p; // функции будет р-1
+    }
+
+    return result;
+}
+
+int modexp(int basis, int power, int modP) {
+    if (power == 0) {
+        return 1;
+    }
+    int z = modexp(basis % modP, power / 2, modP) % modP;
+    if (power % 2 == 0) {
+        return (z * z) % modP;
+    }
+    else {
+        return ((basis % modP) * ((z * z) % modP)) % modP;
+    }
+}
+
+pair<int, int> remainder(int& basis, int& power, int& modP) {
+    pair<int, int> ost = { 1,1 };
+
+    ost.first = modexp(basis, power, modP);
+
+    int logByNumber = log2(power);     //рассматриваем остатки через логарифм и 2сс.
+    vector<int> binary;  //переводим степень в 2сс.
+    int power2 = power;
+    while (power2 > 0) {
+        binary.push_back(power2 % 2);
+        power2 /= 2;
+    }
+
+    int resultForPrime = 1;
+    for (int i = 0; i <= logByNumber; i++) {
+        if (binary[i] == 1) { //бинарное не переворачиваем, а просто идем с "конца"
+            int oneDegree = pow(2, i);
+            resultForPrime *= modexp(basis, oneDegree, modP);
+        }
+    }
+    ost.second = resultForPrime % modP;  //после смотрим на остаток
+
+    return ost;  //вернем пару значений
+}
+
+int main() {
+    setlocale(LC_ALL, "RUS");
+    int basis = 0, power = 0, modP = 0;
+    cout << "Введите основание числа: ";
+    cin >> basis;
+    cout << "Введите степень числа: ";
+    cin >> power;
+    cout << "Введите простое число - модуль числа: ";
+    cin >> modP;
+
+    if (basis < modP && power == (modP - 1) && (Prime(modP) == true)) { // Воспользуемся теоремой Ферма
+        cout << "Согласно теореме Ферма получим: " << basis << "^" << power << " mod " << modP << " = 1" << endl;
+        pair<int, int> ost = remainder(basis, power, modP);  //рассматриваем остатки через функцию
+        cout << "Проверим, используя логарифм: " << ost.second << endl;
+        return 0;
+    }
+    if (NOD(basis, modP) == 1) { // если числа взаимнопростые, можем воспользоваться теореой Эйлера
+        if (power == EilerFunc(modP)) {
+            cout << "Согласно теореме Эйлера получим: " << basis << "^" << power << " mod " << modP << " = 1" << endl;
+            pair<int, int> ost = remainder(basis, power, modP);  //рассматриваем остатки через функцию
+            cout << "Проверим, используя логарифм: " << ost.second << endl;
+            return 0;
+        }
+        else {
+            power == EilerFunc(modP);
+            int result = modexp(basis, power, modP);
+            pair<int, int> ost = remainder(basis, power, modP);  //рассматриваем остатки через функцию
+            cout << "Результат, используя свойства сравнений = " << ost.first << " и результат, используя логарифм = " << ost.second << endl;
+            return 0;
+        }
+    }
+    cout << "Число делится без остатка ";
+    return 0;
+}
---
+++ practice2.2.cpp
@@ -0,0 +1,33 @@
+﻿// the generic Euclid algorithm
+// 2nd task 
+
+#include <iostream>
+#include <vector>
+using namespace std;
+
+vector <int> euclid(int a, int b, int& x, int& y) {
+    vector <int> A{ a, 1, 0 };
+    vector <int> B{ b, 0, 1 };
+    cout << A[0] << "\t" << A[1] << "\t" << A[2] << endl; //выводим текущие картежи
+    cout << B[0] << "\t" << B[1] << "\t" << B[2] << endl;
+    while (B[0] != 0) {
+        int integer = A[0] / B[0]; //целая часть от деления чисел в картеже
+        vector<int> remains{ A[0] % B[0], A[1] - integer * B[1], A[2] - integer * B[2] };
+        cout << remains[0] << "\t" << remains[1] << "\t" << remains[2] << "\t" << integer << endl;
+        A = B;  //в первый картеж закидываем второй 
+        B = remains;     //во второй закидываем с остатком
+    }
+    return A;
+}
+
+
+int main() {
+    int a, b;
+    cout << "Enter two numbers:" << endl;
+    cin >> a >> b;
+    int x = 0, y = 0;
+    vector <int> result = euclid(a, b, x, y);
+    cout << "GCD:" << endl;
+    cout << a << " * (" << result[1] << ") + " << b << " * (" << result[2] << ") = " << result[0] << endl;
+    return 0;
+}
